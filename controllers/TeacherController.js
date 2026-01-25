d839a026bce708a2095f09b32a56dce7d2e9d7fc
MARIO MACHACA <mariomachaca5477@gmail.com>
2026-01-24 23:57:19 -0400

    feat: add consult teachers in TeacherController

--- controllers/TeacherController.js
+++ controllers/TeacherController.js
@@ -4,7 +4,22 @@ class TeacherController {
   constructor() {}
 
   consult(req, res) {
-    res.json({ msg: 'Consult teachers from class' });
+    try {
+      const query = `SELECT * FROM teachers;`;
+      db.query(query, (err, rows) => {
+        if (err) {
+          return res.status(400).send(err);
+        }
+        if (rows.length === 0) {
+          return res
+            .status(400)
+            .json({ msg: 'There are no registered teachers' });
+        }
+        res.status(200).send(rows);
+      });
+    } catch (err) {
+      res.status(500).send(err);
+    }
   }
 
   insert(req, res) {
